9188e146bf7c926ce5cab17047e091dd499ac896
java0821 <java0821@example.com>
2022-04-16 23:48:10 +0800

    呛

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
       <p> 呛 </p>
       <p> 落日飞花 </p>
-=======
-      <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
-      <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
-      <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
-      <h1>祝 前程似锦</h1>
-
->>>>>>> 35dd494bd8e9b76ca323200069a2ff8b579fbcf1
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
